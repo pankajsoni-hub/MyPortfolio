b3ba48869a549c563856d0caa6b2f8e5eaf08f92
Pankaj Soni <85389661+pankajsoni-hub@users.noreply.github.com>
2024-12-19 22:16:13 +0530

    first commit

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -21,7 +21,7 @@ const ContactForm: FC = memo(() => {
       try {
         console.log('Form data to send:', formData);
         // Replace this with your API logic for form submission
-        const response = await fetch('http://localhost:5000/api/contact', {
+        const response = await fetch('https://my-portfolio-gray-zeta-14.vercel.app/api/contact', {
           method: 'POST',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify(formData),
